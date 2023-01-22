c400f1ac964731fb8c4dc83e517ed83299821eb2
Richard Mills <me@richardmills.ca>
2023-01-22 14:14:21 -0800

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./templates/**/*.html",
+    "./templates/shortcodes/**/*.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
